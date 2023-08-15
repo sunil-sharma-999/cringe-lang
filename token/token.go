c5d04f4b24615c41aa079317f73b239256f602b2
sunil <iamsunilsharma999@gmail.com>
2023-08-15 10:54:42 +0530

    add tokens

--- token/token.go
+++ token/token.go
@@ -5,3 +5,24 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
