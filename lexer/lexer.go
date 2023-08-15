a388b7e6976649eb6fc9321f22ba50a34e1579a7
sunil <iamsunilsharma999@gmail.com>
2023-08-15 11:01:16 +0530

    add readChar method on Lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -11,3 +11,14 @@ func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
 }
+
+// read character function
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
